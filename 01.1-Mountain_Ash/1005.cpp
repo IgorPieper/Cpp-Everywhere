c28f2319cc1b09952820904ed8ac018ca4484619
IgorPieper <91740800+IgorPieper@users.noreply.github.com>
2021-11-06 17:05:01 +0100

    Add files via upload

---
+++ 01.1-Mountain_Ash/1005.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <math.h>
+
+auto main () -> int{
+
+int liczba1;
+
+
+std::cout<<"Proszę podać 1 liczbe: ";
+std::cin>>liczba1;
+
+std::cout<<"Wartość absolutna z podanej liczby wynosi: "<<abs(liczba1)<<"\n";
+
+return 0;
+
+}
