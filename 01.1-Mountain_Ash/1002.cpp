2180b5ee62b9515a449d5644a0e60abff7172799
IgorPieper <91740800+IgorPieper@users.noreply.github.com>
2021-11-06 16:57:54 +0100

    Dodanie Dodawania

---
+++ 01.1-Mountain_Ash/1002.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+auto main () -> int{
+
+int liczba1;
+int liczba2;
+
+std::cout<<"Proszę podać 1 liczbe: ";
+std::cin>>liczba1;
+std::cout<<"Proszę podać 2 liczbe: ";
+std::cin>>liczba2;
+
+std::cout<<"Wynik Dodawania: "<<liczba1 + liczba2<<"\n";
+
+return 0;
+
+}
