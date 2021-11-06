dbde49db1dec8fdd6d4092e97e0597b9b24428a3
IgorPieper <91740800+IgorPieper@users.noreply.github.com>
2021-11-06 20:31:18 +0100

    2012

---
+++ 01.2-Bilberry/2012.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+auto main () -> int{
+
+int wysokosc;
+
+std::cout<<"Wysokość prostokąta wynosi: ";
+std::cin>>wysokosc;
+
+for (int i=1;i<=wysokosc;i++){
+	for(int ii=1;ii<=i;ii++){
+		std::cout<<"* ";
+	}
+	std::cout<<"\n";
+}
+
+return 0;
+
+}
